315d604752123d34906b59e55569dee6b41fa590
Eduardo Bezerra <eduardocarioca55@hotmail.com>
2017-10-25 21:17:24 -0300

    alteracoes

--- wp-content/themes/wdjewelry/framework/functions/header_menu.php
+++ wp-content/themes/wdjewelry/framework/functions/header_menu.php
@@ -39,10 +39,10 @@ function wdjewelry_menu_mobile(){?>
 					<img src="<?php echo wdjewelry_get_logo_url();?>" alt='<?php echo esc_html__("logo","wdjewelry");?>'>
 				</a>
 			</div>
-
+<!-- 
 			<div class="mobile-seach">
 				<?php get_search_form(); ?>
-			</div>
+			</div> -->
 		</div>
 <?php } 
 function wdjewelry_menu_main(){ ?>
@@ -51,27 +51,53 @@ function wdjewelry_menu_main(){ ?>
 			<div class="container">
 				<div class="row">
 					<div class="header-top-content">
-						<div class='header-top-left col-md-10 col-sm-12'>
-							<?php 
-								if(is_active_sidebar("header-1" ) ):
-									dynamic_sidebar("header-1" );
-								endif;
-							?>
-						</div>
-						<div class="header-top-right col-md-14 col-sm-12">
-							<?php 
-								if(is_active_sidebar("header-2" ) ):
-									dynamic_sidebar("header-2" );
-								endif;
-							?>
+						<?php 
+							if(is_active_sidebar("header-2" ) ):
+								dynamic_sidebar("header-2" );
+							endif;
+								
+						?>
+						
+							<div class="header-main-logo">
+								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" >
+								<img src="<?php echo wdjewelry_get_logo_url();?>" alt='<?php echo esc_html__("logo","wdjewelry");?>'>
+							</div>
+						<div class="">
+
+							<?php if(get_theme_mod('show_wishlist',true)): ?>
+							<div class='header-top-right-wishlist'></div>
+							<?php endif; ?>
+
+							<?php if(get_theme_mod('show_cart',true)): ?>
+							<div class='header-top-right-account'><?php echo wdjewelry_mini_cart();?></div>
+							<?php endif; ?>
+
 							<?php if(get_theme_mod('show_account',true)): ?>
 							<div class='header-top-right-account'><?php echo wdjewelry_woo_account();?></div>
 							<?php endif; ?>
-							<?php if(get_theme_mod('show_wishlist',true)): ?>
-							<div class='header-top-right-wishlist'><?php echo wdjewelry_wd_tini_wishlist(); ?></div>
+
+
+
+
+
+
+
+
+
+
+
+							<?php if(get_theme_mod('show_seach',true)): ?>
+
+							<div class='seach' style="float: right !important;">
+								<span class='seach__icon'  style="float: right !important;"></span>
+								<?php get_search_form(); ?>
+							</div>
+							
 							<?php endif; ?>
 
+						
 						</div>
+						
 						<div class="clear"></div>
 					</div>
 				</div>
@@ -80,47 +106,38 @@ function wdjewelry_menu_main(){ ?>
 		<div class="clear"> </div>
 
 		<div class='site-header-main'>
-			<div class="container">
-				<div class="row">
-					<div class='header-main-content'>
-						<div class="header-main-logo col-md-6 col-sm-6">
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>" >
-							<img src="<?php echo wdjewelry_get_logo_url();?>" alt='<?php echo esc_html__("logo","wdjewelry");?>'>
-							</a>
-						</div>
-						<div class='header-main-menu col-md-18 col-sm-18'>
-							<div class='header-main-menu__custom'>
-							<?php 
-								if (has_nav_menu('primary')):
-								wp_nav_menu(array("theme_location" => "primary",'menu_class' => 'primary nav-menu',
-									"container" => "div" ));
-								else: 
-								$args = array(
-									'menu_class' => 'primary nav-menu',
-									"container" => "div", 
-								);
-								wp_nav_menu( $args );
-								endif;  
-							?>
+					<div class="container">
+						<div class="row">
+							<div class='header-main-content'>
+								
+								<div class='header-main-menu col-md-18 col-sm-offset-4'>
+									<div class='header-main-menu__custom'>
+									<?php 
+										if (has_nav_menu('primary')):
+										wp_nav_menu(array("theme_location" => "primary",'menu_class' => 'primary nav-menu',
+											"container" => "div" ));
+										else: 
+										$args = array(
+											'menu_class' => 'primary nav-menu',
+											"container" => "div", 
+										);
+										wp_nav_menu( $args );
+										endif;  
+									?>
+									</div>
+								</div>
 							</div>
+							
+							<div class='clear'></div>
+
 						</div>
 					</div>
-					<?php if(get_theme_mod('show_cart',true)): ?>
-					<div class='header-top-right-cart'><?php echo wdjewelry_mini_cart();?></div>
-					<?php endif; ?>
-					<?php if(get_theme_mod('show_seach',true)): ?>
-
-					<div class='seach' style="float: right !important;">
-						<span class='seach__icon'></span>
-						<?php get_search_form(); ?>
-					</div>
-					
-					<?php endif; ?>
-					<div class='clear'></div>
-
-				</div>
-			</div>
 		</div>
+
+
+
+
+		
 	</div>
 
 <?php }
--- wp-content/themes/wdjewelry/searchform.php
+++ wp-content/themes/wdjewelry/searchform.php
@@ -10,7 +10,7 @@
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label>
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'wdjewelry' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'wdjewelry' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'wdjewelry' ); ?>" />
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Procurar &hellip;', 'placeholder', 'wdjewelry' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'wdjewelry' ); ?>" />
 	</label>
 	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'wdjewelry' ); ?></span></button>
 </form>
